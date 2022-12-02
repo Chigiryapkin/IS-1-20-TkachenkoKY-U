abdc1f3b62004262d5cef79a00bf925418fa7314
cchuc.para@mail.ru <cchuc.para@mail.ru>
2022-12-02 18:00:27 +0500

    Добавьте файлы проекта.

---
+++ IS-3-20-TkachenkoKY-U/Task1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IS_3_20_TkachenkoKY_U
+{
+    public partial class One : Form
+    {
+        public One()
+        {
+            InitializeComponent();
+        }
+        public abstract class Components<P>
+        {
+            public P articul;
+            public int price;
+            public int date;
+            public Components(int Price, int Date, P Articul)
+            {
+                price = Price;
+                date = Date;
+                articul = Articul;
+            }
+            public void Display()
+            {
+                MessageBox.Show($"Артикул : {articul}\nЦена:{price}\nГод выпуска:{date}");
+            }
+        }
+        public class Harddrive<P>
+        {
+            public int turnovers { get; set; }
+            public string interfac { get; set; }
+            public int volume { get; set; }
+
+            public Harddrive(int Price , int Date , int Turnovers , string Interfac , int Volume , P Articul) : base(Price , Date , Articul)
+            {
+                turnovers = Turnovers;
+                interfac = Interfac;
+                volume = Volume;
+
+            }
+            public new void Display()
+            {
+                MessageBox.Show($"Артикул : {Articul} \nЦена:{Price}\nГод выпуска: {Date}\nКоличество оборотов: {turnovers}\nИнтерфейс: {interfac}\nОбъём: {volume}");
+            }
+        }
+
+        private void One_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
